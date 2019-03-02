99e5d8da09b95d7bfe6a6b1f19e418df72895555
rachelferrer <rferrer@beaconschool.org>
2019-03-02 16:48:18 +0000

    done

--- problemSetDay4.py
+++ problemSetDay4.py
@@ -4,3 +4,9 @@ while strength <= 10:
     print(strength)
     strength += 1
 print('You have grown too strong! Time for a new level!')
+strength = 10
+print('Your strength is 10!')
+while strength >= 100:
+    print(strength)
+    strength -= 10
+print('You have grown too weak! You must power up!')
